87bd61f6fcdb292811ac9305c6e08c4c07071436
Rudinei Costa <costaaugusto177@outlook.com>
2022-11-11 16:37:59 -0300

    wip: routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 import Fastify from 'fastify';
 import {PrismaClient} from '@prisma/client';
 import cors from '@fastify/cors';
-
+import {z} from 'zod'
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log:['query',]
@@ -20,6 +21,37 @@ async function bootstrap(){
      const pools = await prisma.pool.count()
     return {pools}
   })
+
+  fastify.post('/pools', async (request, reply)=>{
+    const createPoolBody = z.object({
+      title:z.string(),
+    })
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({length: 6});
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data:{
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({title, code})
+ })
+
+ fastify.get('/users/count', async ()=>{
+  const users = await prisma.user.count()
+ return {users}
+})
+
+fastify.get('/guesses/count', async ()=>{
+  const guesses = await prisma.guess.count()
+ return {guesses}
+})
+
+
 
   // await fastify.listen({port:3333, host: '0.0.0.0'})
   await fastify.listen({port:3333})
